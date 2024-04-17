df319ba1be376decbc05e361c5764ba912aafd57
Lbokhua11 <l.bokhua20@gmail.com>
2024-04-17 18:05:36 +0400

    gamarjoba

--- components/shared/Container/OrderContainer.tsx
+++ components/shared/Container/OrderContainer.tsx
@@ -16,6 +16,7 @@ const OrderContainer = ({ orderId }: Props) => {
       <main className="w-full h-vh-minus-header-footer absolute top-[4rem]">
         <section className="w-[26rem] h-[36.75rem]  absolute top-[4.75rem] left-1/2 -translate-x-1/2 flex flex-col rounded-[0.625rem, 0.625rem, 0rem, 0rem]">
           <div className="h-[5.5rem] w-[26rem] bg-green-500  p-[1rem, 1.25rem, 1rem, 1.25rem] rounded-t-[0.625rem, 0.625rem]"></div>
+          <h1>gamarjoba</h1>
         </section>
       </main>
 
